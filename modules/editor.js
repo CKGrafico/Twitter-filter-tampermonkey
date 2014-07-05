f689126409d8605ea5a0cbcfdcceaea9d5a92804
Quique Fdez Guerra <quique@ckgrafico.com>
2014-07-05 20:03:15 +0200

    add input styles

--- modules/editor.js
+++ modules/editor.js
@@ -13,6 +13,13 @@
 	};
 
 	/**
+	 * Bind events of this module
+	 */
+	function bindEvents() {
+		$('.ck-filter-close, .ck-filters').on('click', togglePanel);
+	}
+
+	/**
 	 * Create config button and append in twitter menu
 	 */
 	function createButton() {
@@ -21,7 +28,14 @@
 		var $a = $('<a/>').addClass('js-nav').attr('href', '#').attr('title', 'Filter');
 		var $span1 = $('<span/>').addClass('Icon Icon--newsBadge Icon--large');
 		var $span2 = $('<span/>').addClass('text').text('Filters');
-		$li.append($a.append($span1).append($span2)).appendTo($container);
+
+		$li
+			.append(
+				$a
+					.append($span1)
+					.append($span2)
+				)
+			.appendTo($container);
 	}
 
 	/**
@@ -30,19 +44,39 @@
 	function createPanel() {
 		$container = $('<div/>').addClass('modal-container ck-filter-container');
 		$close = $('<div/>').addClass('close-modal-background-target ck-filter-close');
-		$modal = $('<div/>').addClass('modal-content ck-modal-content').text('testong');
+		$modal = $('<div/>').addClass('modal-content ck-modal-content');
+		$h2 = $('<h2/>').text('Filters configuration');
 
-		$container.append($close).append($modal).appendTo($('body'));
+		var $input1 = createInput(
+			{
+				label: 'Twitter accounts',
+				name: 'accounts',
+				placeholder: 'Charles, twitter, hater',
+				description: 'Write twitter accounts to exclude'
+			}
+		);
+
+		var $input2 = createInput(
+			{
+				label: 'Specific words',
+				name: 'words',
+				placeholder: 'hello, money, idiot',
+				description: 'Write specific words to exclude'
+			}
+		);
+
+		$modal
+			.append($h2)
+			.append($input1)
+			.append($input2)
+
+		$container
+			.append($close)
+			.append($modal);
+
+		$('body').append($container);
 	}
 
-	/**
-	 * Bind events of this module
-	 */
-	function bindEvents() {
-		$('.ck-filter-close, .ck-filters').on('click', togglePanel);
-		$('.ck-filter-close').on('click', closePanel);
-		$(".ck-filter-dialog").draggable();
-	}
 
 	/**
 	 * Open/Close configuration panel
@@ -57,6 +91,28 @@
 		}
 
 		$('.ck-filter-container, .modal-overlay').css('display', nextState);
+	}
+
+	/**
+	 * Create an input for the filter
+	 */
+	function createInput(options) {
+
+		$inputContainer = $('<div/>').addClass('control-group');
+		$label = $('<label/>').attr('for', 'ck-input-' + options.name).addClass('t1-label control-label').text(options.label);
+		$controls = $('<div/>').addClass('controls');
+		$input = $('<input/>').attr('id', 'ck-input-' + options.name).attr('type', 'text').attr('placeholder', options.placeholder);
+		$p = $('<p/>').addClass('notification').text(options.description);
+
+		$inputContainer
+			.append($label)
+			.append(
+				$controls
+					.append($input)
+					.append($p)
+			);
+
+		return $inputContainer;
 	}
 
 	this.initializeEditor = initialize;
