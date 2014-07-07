71dac831043988875166b6de51c9c7bd5630bad3
Quique Fdez Guerra <quique@ckgrafico.com>
2014-07-07 15:20:58 +0200

    fixed an finished version 0.0.1

--- build/min/all.min.user.js
+++ build/min/all.min.user.js
@@ -1,19 +1,1 @@
-// GPLv2 Licensed
-// http://www.gnu.org/licenses/gpl-2.0.html
-//
-// ==UserScript==
-// @name            Twitter-Filter
-// @description     Active filter on twitter
-// @namespace       http://ckgrafico.com
-// @version         0.0.1
-// @author          CKGrafico
-// @include         https://www.twitter.com*
-// @include         https://twitter.com*
-// @include         http://www.twitter.com*
-// @include         http://twitter.com*
-// @resource        styles https://raw.githubusercontent.com/CKGrafico/Twitter-filter-tampermonkey/master/build/min/all.min.css
-// @run-at          document-end
-// @require         http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js
-// ==/UserScript==
-
 !function(){!function(){window.requestAnimationFrame||(window.requestAnimationFrame=function(){return window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(e){window.setTimeout(e,1e3/60)}}())}(),GM_addStyle(GM_getResourceText("styles")),initializeEditor(),requestAnimationFrame(filterStream)}(jQuery);
